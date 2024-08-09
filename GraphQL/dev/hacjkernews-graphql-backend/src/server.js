740316be74ed869a2731ee1a935120d38eb5417a
Iryu-0817 <iryu20030817@gmail.com>
2024-08-09 17:41:15 +0900

    リゾルバを管理するためのリファクタリング

--- GraphQL/dev/hacjkernews-graphql-backend/src/server.js
+++ GraphQL/dev/hacjkernews-graphql-backend/src/server.js
@@ -2,33 +2,28 @@ const { ApolloServer, gql } = require("apollo-server");
 const fs = require("fs");
 const path = require("path");
 
-//HackerNewsのデータ
-let links = [
-  {
-    id: "link-0",
-    description: "GtaphQlチュートリアルをUdemyで学ぶ",
-    url: "www.udemy-graphql-tutorial.com",
-  },
-];
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 
 // リゾルバ関数
 const resolvers = {
   Query: {
     info: () => "HackerNews Clone API",
-    feed: () => links,
+    feed: async (parent, args, context) => {
+      return context.prisma.link.findMany();
+    },
   },
 
   //ミュテーションのリゾルバ関数
   Mutation: {
-    post: (parent, args) => {
-      let idCount = links.length;
-      const link = {
-        id: `link-${idCount++}`,
-        description: args.description,
-        url: args.url,
-      };
-      links.push(link);
-      return link;
+    post: (parent, args, context) => {
+      const newLink = context.prisma.link.create({
+        data: {
+          url: args.url,
+          description: args.description,
+        },
+      });
+      return newLink;
     },
   },
 };
@@ -37,6 +32,8 @@ const resolvers = {
 const server = new ApolloServer({
   typeDefs: fs.readFileSync(path.join(__dirname, "schema.graphql"), "utf8"),
   resolvers,
+  // prismaがリゾルバ関数内で利用できるようになる
+  context: () => ({ prisma }),
 });
 
 server
