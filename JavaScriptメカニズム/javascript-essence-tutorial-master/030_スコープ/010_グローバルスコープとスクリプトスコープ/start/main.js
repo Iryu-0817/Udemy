357230e83931478413e12c386136df78ec6b1734
Iryu-0817 <iryu20030817@gmail.com>
2024-11-02 22:26:34 +0900

    スコープ１

---
+++ JavaScriptメカニズム/javascript-essence-tutorial-master/030_スコープ/010_グローバルスコープとスクリプトスコープ/start/main.js
@@ -0,0 +1,5 @@
+let a = 0;
+let b = 0;
+function c() {}
+
+console.log(b); //windowはグローバルスコープであるため、省略してもbが参照できる
---
+++ JavaScriptメカニズム/javascript-essence-tutorial-master/030_スコープ/030_レキシカルスコープ/start/main.js
@@ -0,0 +1,10 @@
+let a = 2;
+
+function fn1() {
+    let b = 1;
+    function f2() {
+        let c = 2;
+    }
+    f2();
+}
+fn1();
