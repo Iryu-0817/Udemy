2d7051437f1fe0422197016e2cbdd26df8028006
Iryu-0817 <iryu20030817@gmail.com>
2025-01-07 14:49:45 +0900

    バイト型

--- GoLang/hello/hello.go
+++ GoLang/hello/hello.go
@@ -3,24 +3,11 @@ package main
 import "fmt"
 
 func main() {
-    n := []int{1, 2, 3, 4, 5, 6}
-    fmt.Println(n)
-    fmt.Println(n[2])
-    fmt.Println(n[2:4])
-    fmt.Println(n[:2])
-    fmt.Println(n[2:])
-    fmt.Println(n[:])
+    b := []byte{72, 73}
+    fmt.Println(b)
+    fmt.Println(string(b))
 
-    n[2] = 100
-    fmt.Println(n)
-
-    var board = [][]int{
-        []int{0, 1, 2},
-        []int{3, 4, 5},
-        []int{6, 7, 8},
-    }
-    fmt.Println(board)
-
-    n = append(n, 100, 200, 300, 500)
-    fmt.Println(n)
+    c := []byte("HI")
+    fmt.Println(c)
+    fmt.Println(string(c))
 }
