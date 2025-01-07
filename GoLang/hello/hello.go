0a0ebdc14aceda87d7df922f6a0516bf72818ce6
Iryu-0817 <iryu20030817@gmail.com>
2025-01-07 16:42:28 +0900

    defer

--- GoLang/hello/hello.go
+++ GoLang/hello/hello.go
@@ -3,35 +3,13 @@ package main
 import "fmt"
 
 func main() {
-    l := []string{"python", "go", "java"}
+    defer fmt.Println("後で実行されます")
+    fmt.Println("最初に実行されます")
 
-    for i := 0; i < len(l); i++{
-        fmt.Println(i, l[i])
-    }
-
-    for i, v := range l{
-        fmt.Println(i, v)
-    }
-
-    // iを使いたくない場合
-    for _, v := range l{
-        fmt.Println(v)
-    }
-
-    // mapを使う場合
-    m := map[string]int{"apple": 100, "banana": 200}
-
-    for k, v := range m{
-        fmt.Println(k, v)
-    }
-
-    // vを使いたくない場合、スライスと違い、＿を使わずに書く。
-    for k := range m{
-        fmt.Println(k)
-    }
-
-    // kを使いたくない場合は記述が必要。つまり最初だけ使いたくない場合は＿を記述する必要がある
-    for _, v := range m{
-        fmt.Println(v)
-    }
+    //実行される順番に注意  
+    fmt.Println("run")
+    defer fmt.Println(1)
+    defer fmt.Println(2)
+    defer fmt.Println(3)
+    fmt.Println("success")
 }
