5dc6f91d42c6aa78d99c269f2a1b017f7759a604
Iryu-0817 <iryu20030817@gmail.com>
2025-01-07 15:57:47 +0900

    if

--- GoLang/hello/hello.go
+++ GoLang/hello/hello.go
@@ -1,23 +1,44 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
-func foo(params ...int) {
-    fmt.Println(len(params), params)
-    for _, param := range params{
-        fmt.Println(param)
-    }
+func by2(num int) string {
+	if num%2 == 0 {
+		return "ok"
+	} else {
+		return "no"
+	}
 }
 
 func main() {
-    foo()
-    foo(10, 20)
-    foo(10, 20, 30)
+	result := by2(10)
+	if result == "ok" {
+		fmt.Println("great")
+	}
+	fmt.Println(result)
 
-    s := []int{1, 2 ,3}
-    fmt.Println(s)
+    // 1行でまとめる際は、result2（代入した値）は外部では使えない
+	if result2 := by2(10); result2 == "ok" {
+		fmt.Println("great 2")
+	}
+	// fmt.Println(result2)
 
-    foo(s...)
+	num := 6
+	if num%2 == 0 {
+		fmt.Println("by 2")
+	} else if num%3 == 0 {
+		fmt.Println("by 3")
+	} else {
+		fmt.Println("else")
+	}
+
+	x, y := 11, 12
+	if x == 10 && y == 10 {
+		fmt.Println("&&")
+	}
+
+	if x == 10 || y == 10 {
+		fmt.Println("||")
+	}
+
 }
