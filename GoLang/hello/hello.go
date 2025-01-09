0016b5c7d35f51f0b4d72b62f4e1ddc1cf802f99
Iryu-0817 <iryu20030817@gmail.com>
2025-01-09 18:14:09 +0900

    Channels

--- GoLang/hello/hello.go
+++ GoLang/hello/hello.go
@@ -1,30 +1,17 @@
 package main
 
-import (
-	"fmt"
-	"sync"
-)
-
-func goroutine(s string, wg *sync.WaitGroup) {
-	defer wg.Done()
-	for i := 0; i < 5; i++ {
-		//time.Sleep(100 * time.Millisecond)
-		fmt.Println(s)
-	}
-}
-
-func normal(s string) {
-	for i := 0; i < 5; i++ {
-		//time.Sleep(100 * time.Millisecond)
-		fmt.Println(s)
-	}
-}
+import "fmt"
 
 func main() {
-	var wg sync.WaitGroup
-	wg.Add(1)
-	go goroutine("world", &wg)
-	normal("hello")
-	// time.Sleep(2000 * time.Millisecond)
-	wg.Wait()
+	ch := make(chan int, 2)
+	ch <- 100
+	fmt.Println(len(ch))
+	ch <- 200
+	fmt.Println(len(ch))
+	close(ch)
+
+	for c := range ch {
+		fmt.Println(c)
+	}
+
 }
