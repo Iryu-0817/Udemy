e07922821cd18ec1105c44ecccc95f23deb78b42
Iryu-0817 <iryu20030817@gmail.com>
2025-01-09 16:07:07 +0900

    コンストラクタ

--- GoLang/hello/hello.go
+++ GoLang/hello/hello.go
@@ -3,58 +3,30 @@ package main
 import "fmt"
 
 type Vertex struct {
-	X, Y int
-	S    string
+	x, y int
 }
 
-func changeVertex(v Vertex) {
-	v.X = 1000
+func (v Vertex) Area() int {
+	return v.x * v.y
 }
 
-func changeVertex2(v *Vertex) {
-	// (*v).X = 1000 <-本来ならこう書くべきかもしれないが、ストラクトでは自動的に解釈される。
-	v.X = 1000
+func (v *Vertex) Scale(i int) {
+	v.x = v.x * i
+	v.y = v.y * i
+}
+
+//func Area(v Vertex) int {
+//	return v.x * v.y
+//}
+
+func New(x, y int) *Vertex {
+	return &Vertex{x, y}
 }
 
 func main() {
-	v := Vertex{X: 1, Y: 2}
-	fmt.Println(v)
-	fmt.Println(v.X, v.Y)
-	v.X = 100
-	fmt.Println(v.X, v.Y)
-
-	v2 := Vertex{X: 1}
-	fmt.Println(v2)
-
-	v3 := Vertex{1, 2, "test"}
-	fmt.Println(v3)
-
-	v4 := Vertex{}
-	fmt.Printf("%T %v\n", v4, v4)
-
-    //varで宣言するとnilにはならない
-	var v5 Vertex
-	fmt.Printf("%T %v\n", v5, v5)
-
-	v6 := new(Vertex)
-	fmt.Printf("%T %v\n", v6, v6)
-
-    // newと同じやり方（＆があるから、アドレスが返ってくることがわかりやすい。）
-	v7 := &Vertex{}
-	fmt.Printf("%T %v\n", v7, v7)
-
-	/*
-		    s := make([]int, 0)
-			s := []int{}
-			fmt.Println(s)
-	*/
-
-	v8 := Vertex{1, 2, "test"}
-	changeVertex(v8)
-	fmt.Println(v8)
-
-	v9 := &Vertex{1, 2, "test"}
-	changeVertex2(v9)
-	fmt.Println(v9)
-
+	// v := Vertex{3, 4}
+	// fmt.Println(Area(v))
+	v := New(3, 4)
+	v.Scale(10)
+	fmt.Println(v.Area())
 }
