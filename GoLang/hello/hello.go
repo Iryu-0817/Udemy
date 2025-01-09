1025d0671b02f56ef8f425c2ef0056a01d29829c
Iryu-0817 <iryu20030817@gmail.com>
2025-01-09 14:34:30 +0900

    ストラクト

--- GoLang/hello/hello.go
+++ GoLang/hello/hello.go
@@ -2,13 +2,59 @@ package main
 
 import "fmt"
 
+type Vertex struct {
+	X, Y int
+	S    string
+}
+
+func changeVertex(v Vertex) {
+	v.X = 1000
+}
+
+func changeVertex2(v *Vertex) {
+	// (*v).X = 1000 <-本来ならこう書くべきかもしれないが、ストラクトでは自動的に解釈される。
+	v.X = 1000
+}
+
 func main() {
-    s := make([]int, 0)
-    fmt.Printf("%T\n", s)
+	v := Vertex{X: 1, Y: 2}
+	fmt.Println(v)
+	fmt.Println(v.X, v.Y)
+	v.X = 100
+	fmt.Println(v.X, v.Y)
 
-    m := make(map[string]int)
-    fmt.Printf("%T\n", m)
+	v2 := Vertex{X: 1}
+	fmt.Println(v2)
 
-    var p *int = new(int)
-	fmt.Printf("%T\n", p)
+	v3 := Vertex{1, 2, "test"}
+	fmt.Println(v3)
+
+	v4 := Vertex{}
+	fmt.Printf("%T %v\n", v4, v4)
+
+    //varで宣言するとnilにはならない
+	var v5 Vertex
+	fmt.Printf("%T %v\n", v5, v5)
+
+	v6 := new(Vertex)
+	fmt.Printf("%T %v\n", v6, v6)
+
+    // newと同じやり方（＆があるから、アドレスが返ってくることがわかりやすい。）
+	v7 := &Vertex{}
+	fmt.Printf("%T %v\n", v7, v7)
+
+	/*
+		    s := make([]int, 0)
+			s := []int{}
+			fmt.Println(s)
+	*/
+
+	v8 := Vertex{1, 2, "test"}
+	changeVertex(v8)
+	fmt.Println(v8)
+
+	v9 := &Vertex{1, 2, "test"}
+	changeVertex2(v9)
+	fmt.Println(v9)
+
 }
