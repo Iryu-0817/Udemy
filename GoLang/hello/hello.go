77ed6ba4d3f2947eba697d2fa6809bf4de9acb84
Iryu-0817 <iryu20030817@gmail.com>
2025-01-09 20:51:00 +0900

    for select

--- GoLang/hello/hello.go
+++ GoLang/hello/hello.go
@@ -5,32 +5,22 @@ import (
 	"time"
 )
 
-func goroutine1(ch chan string) {
-	for {
-		ch <- "packet from 1"
-		time.Sleep(3 * time.Second)
-	}
-}
-
-func goroutine2(ch chan int) {
-	for {
-		ch <- 100
-		time.Sleep(1 * time.Second)
-	}
-}
-
 func main() {
-	c1 := make(chan string)
-	c2 := make(chan int)
-	go goroutine1(c1)
-	go goroutine2(c2)	
+	tick := time.Tick(100 * time.Millisecond)
+	boom := time.After(500 * time.Millisecond)
+	// for selectの抜け出し方
+	OuterLoop2:
+		for {
+			select {
+			case <- tick:
+				fmt.Println("tick.")
+			case <- boom:
+				fmt.Println("Boom!")
+				break OuterLoop2
 
-	for {
-		select {
-		case msg1 := <-c1:
-			fmt.Println(msg1)
-		case msg2 := <-c2:
-			fmt.Println(msg2)
+			default:
+				fmt.Println("   .")
+				time.Sleep(50 * time.Millisecond)
+			}
 		}
-	}
 }
